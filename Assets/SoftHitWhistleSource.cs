de92b63cc7d1a41e0424092be618783f8f3cd3e0
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-21 20:15:09 -0400

    Add sound

---
+++ Assets/SoftHitWhistleSource.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftHitWhistleSource : MonoBehaviour
+{
+    [Header("Hitsound taken from apple's mapset of Fractal Dreamers - Paradigm Shift")]
+    [SerializeField] string source; // source will be pasted here, but also in comment below.
+    // https://osu.ppy.sh/beatmapsets/716441#osu/1517726
+}
