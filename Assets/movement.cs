7031a3026581b361394853a811ee7bf82bd4a526
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-22 11:45:25 -0400

    Quit range

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,11 +6,12 @@ using UnityEngine.InputSystem;
 public class movement : MonoBehaviour
 {
     Vector2 var1;
+    [SerializeField] int quitRange;
     // Start is called before the first frame update
     void OnMove(InputValue input)
     {
         var1 = input.Get<Vector2>();
-        if (transform.position.magnitude > 10) { Application.Quit(); }
+        if (transform.position.magnitude > quitRange) { Application.Quit(); }
     }
 
     // Update is called once per frame
